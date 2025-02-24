243bf5105916e585d890746b4baf2974341b355d
Kandy-Hamisi <hkandy2000@gmail.com>
2025-02-24 22:11:22 +0300

    removed profilecard on dashboard homepage

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,14 +1,14 @@
-import { ProfileCard } from '@/components'
-import React from 'react'
+import { ProfileCard } from "@/components";
+import React from "react";
 
 const DashboardPage = () => {
   return (
-    <div className='flex gap-6 pt-6'>
-      <div className='hidden xl:block w-[20%]'>
-        <ProfileCard />
+    <div className="flex gap-6 pt-6">
+      <div>
+        <h1>There will be something nice in this page</h1>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default DashboardPage
+export default DashboardPage;
